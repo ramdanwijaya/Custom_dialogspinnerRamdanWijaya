b50cd837803fe9d92b817cd60cd76ccdbf182c46
ramdanwijaya <ramdan.zakat1112@gmail.com>
2022-06-18 19:51:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/custom_dialogspinnerramdanwijaya/ItemSpinner.kt
@@ -0,0 +1,11 @@
+package com.example.custom_dialogspinnerramdanwijaya
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemSpinner : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_spinner)
+    }
+}
